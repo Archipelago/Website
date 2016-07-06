2e5eef01538e7239f37af2913b811e0910b83d88
Niovad <niovad@safe-mail.net>
2016-07-06 20:30:34 +0200

    Add registration
        No error message is displayed now
        No ajax used now

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+let request = require('request');
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
@@ -12,6 +13,21 @@ app.listen(process.env.PORT || config.port);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static('./public'));
+
+app.post('/register', function(req, res) {
+  request.post({url: 'http://localhost:8080/register',
+		form: JSON.stringify(req.body)},
+	       function(e, r, b) {
+		 if (r.statusCode === 201)
+		   res.render('home', {notice: 'Account successfully created'});
+		 else
+		   res.render('register', {error: b.message});
+	       });
+});
+
+app.get('/register', function(req, res) {
+  res.render('register');
+});
 
 app.get('/', function(req, res) {
   res.render('home');
