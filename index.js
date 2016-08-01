d79f6d3492e8866ee702362b977d0e559dbd1288
Niovad <niovad@safe-mail.net>
2016-08-01 08:17:40 +0200

    Add route /movie/:id
        It only displays informations about a movie

--- index.js
+++ index.js
@@ -44,6 +44,25 @@ app.get(['/register', '/login'], function(req, res) {
   res.renderView('register');
 });
 
+// TODO: we must also retrieve releases and links
+app.get('/movie/:id', function(req, res) {
+  apiRequest(req, res, 'get', '/movie/' + req.params.id, function(e, r, b) {
+    b = JSON.parse(b);
+    if (r.statusCode === 200) {
+      let movie = {
+	image: b.image,
+	title: b.title
+      };
+      delete b.image;
+      delete b.title;
+      movie.data = b;
+      res.renderView('movie', {movie: movie});
+    }
+    else
+      res.redirect('/'); // DO NOT DO THIS ON 404
+  });
+});
+
 app.get('/', function(req, res) {
   apiRequest(req, res, 'get', '/movies/last/15', function(e, r, b) {
     if (r.statusCode === 200) {
