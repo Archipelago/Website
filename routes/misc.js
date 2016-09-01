b0598ddd20d0295c5dcdad7f392e8b74bb49125c
Niovad <niovad@safe-mail.net>
2016-09-01 14:50:54 +0200

    Fix css for permissions

--- routes/misc.js
+++ routes/misc.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   });
 
   app.get('/permissions.css', function(req, res) {
-    res.write('[class^=\'permission-\'],[class*=\' permission-\']{display:none}');
+    res.write('[class^=\'permission-\'],[class*=\' permission-\']{display:none !important}');
     let permissions = token.getPermissions(req);
     for (let i = 0; i < permissions.length; ++i) {
       res.write('.permission-' + permissions[i].toLowerCase().replace(/_/g, '-'));
@@ -12,7 +12,7 @@ module.exports = function(app) {
 	res.write(',');
     }
     if (permissions.length > 0)
-      res.write('{display:initial}');
+      res.write('{display:initial !important}');
     res.end();
   });
 }
