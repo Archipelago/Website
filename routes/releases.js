e8540d2e3bb5aa94cda0bd557b0a375c44880def
Niovad <niovad@safe-mail.net>
2016-12-11 16:52:51 +0000

    Add POST route for adding releases

--- routes/releases.js
+++ routes/releases.js
@@ -7,4 +7,15 @@ module.exports = function(app) {
       res.renderView('release_add', {movie_id: req.params.id, lists: b});
     });
   });
+
+  app.post('/movie/:id/release', function(req, res) {
+    apiRequest(req, res, 'post', '/movie/' + req.params.id + '/release', function(e, r, b) {
+      if (r.statusCode === 201) {
+	token.setMessage(req, 'success', 'Release "' + req.body.name + '" successfully added');
+	res.redirect('/movie/' + req.params.id);
+      }
+      else
+	res.redirect('/movie/' + req.params.id + '/release');
+    });
+  });
 };
