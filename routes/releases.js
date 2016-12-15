9c7074f2bc5aabd58f184fa6abe61a7ea947bb74
Niovad <niovad@safe-mail.net>
2016-12-15 00:24:01 +0000

    Add links and releases with the same form

--- routes/releases.js
+++ routes/releases.js
@@ -9,10 +9,26 @@ module.exports = function(app) {
   });
 
   app.post('/movie/:id/release', function(req, res) {
+    let links = [];
+    let name = req.body.name;
+    for (i in req.body) {
+      if (i.match(/^links|\d+$/)) {
+	let splits = req.body[i].split(/\r?\n/g);
+	links.push(splits.length == 1 ? splits[0] : splits);
+	delete req.body[i];
+      }
+    }
     apiRequest(req, res, 'post', '/movie/' + req.params.id + '/release', function(e, r, b) {
       if (r.statusCode === 201) {
-	token.setMessage(req, 'success', 'Release "' + req.body.name + '" successfully added');
-	res.redirect('/movie/' + req.params.id);
+	req.body = links;
+	apiRequest(req, res, 'post', '/video_release/' + b.id + '/link', function(e, r, b) {
+	  if (r.statusCode === 201) {
+	    token.setMessage(req, 'success', 'Release "' + name + '" and links successfully added');
+	    res.redirect('/movie/' + req.params.id);
+	  }
+	  else
+	    res.redirect('/movie/' + req.params.id + '/release');
+	});
       }
       else
 	res.redirect('/movie/' + req.params.id + '/release');
