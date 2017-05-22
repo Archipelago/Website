5278f44f039dc6234635c8d894b6e7986b1c8634
Niovad <niovad@safe-mail.net>
2017-05-22 14:29:04 +0100

    Pre-fill dropdown fields after release submission failure
    	 Set of links are not filled yet
    	 Refactored the whole form

--- routes/releases.js
+++ routes/releases.js
@@ -4,11 +4,12 @@ module.exports = function(app) {
   app.get('/movie/:id/release', function(req, res) {
     apiRequest(req, res, 'get', '/lists', function(e, r, b) {
       // TODO: redirect if permissions failure
-      res.renderView('release_add', {movie_id: req.params.id, lists: b});
+      res.renderView('release_add', {movie_id: req.params.id, lists: b, release: {}});
     });
   });
 
   app.post('/movie/:id/release', function(req, res) {
+    let raw = JSON.parse(JSON.stringify(req.body));
     let links = [];
     let name = req.body.name;
     for (i in req.body) {
@@ -26,12 +27,18 @@ module.exports = function(app) {
 	    token.setMessage(req, 'success', 'Release "' + name + '" and links successfully added');
 	    res.redirect('/movie/' + req.params.id);
 	  }
-	  else
-	    res.redirect('/movie/' + req.params.id + '/release');
+	  else {
+	    apiRequest(req, res, 'get', '/lists', function(e, r, b) {
+	      res.renderView('release_add', {movie_id: req.params.id, lists: b, release: raw});
+	    });
+	  }
 	});
       }
-      else
-	res.redirect('/movie/' + req.params.id + '/release');
+      else {
+	apiRequest(req, res, 'get', '/lists', function(e, r, b) {
+	  res.renderView('release_add', {movie_id: req.params.id, lists: b, release: raw});
+	});
+      }
     });
   });
 };
